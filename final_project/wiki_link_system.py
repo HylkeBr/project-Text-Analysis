eb7c86e4342ab48a1345db4c7f4ea5530a1fb52b
HylkeBr <h.h.brouwer.1@student.rug.nl>
2022-05-25 21:21:35 +0200

    new version of wiki system

--- final_project/wiki_link_system.py
+++ final_project/wiki_link_system.py
@@ -1,9 +1,11 @@
 import os
+from re import search
 import sys
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.parse import CoreNLPParser
-import wikipedia
+# import wikipedia
+from mediawiki import MediaWiki
 
 
 def cleanup_list(data_list):
@@ -23,19 +25,28 @@ def cleanup_list(data_list):
     return data
 
 
-def convert_POS(nltk_POS_tag):
-    if "NN" in nltk_POS_tag:
-        return "n"
-    elif "JJ" in nltk_POS_tag:
-        return "a"
-    elif "RB" in nltk_POS_tag:
-        return "r"
-    elif "VB" in nltk_POS_tag:
-        return "v"
+def find_search_term(file_cont, current_word, search_list):
+    word_columns = current_word.split()
+    next_w_i = file_cont.index(current_word) + 1
+    next_word = file_cont[next_w_i]
+    next_word_col = file_cont[next_w_i].split()
+
+    search_list = search_list + [word_columns[3]]
+
+    if len(next_word_col) != 6:
+        return search_list
+    elif word_columns[5] != next_word_col[5]:
+        return search_list
+    elif next_word == file_cont[-1]:
+        search_list = search_list + [next_word_col[3]]
+        return search_list
     else:
-        return False
+        return find_search_term(file_cont, next_word, search_list)
+
 
 def main():
+    wikipedia = MediaWiki()
+
     content_dict = {}
     dir_names = os.listdir(sys.argv[1])
     for dir in dir_names:
@@ -43,30 +54,26 @@ def main():
             file_content = f.readlines()
         content_dict[dir] = cleanup_list(file_content)
     
-    ######################################
-    # LEMMATIZATION & ENTITY RECOGNITION #
-    ######################################
+    #############################################
+    # ENTITY RECOGNITION + WIKI-LINK ASSIGNMENT #
+    #############################################
 
-    lemmatizer = WordNetLemmatizer()
     ner_tagger = CoreNLPParser(url="http://localhost:9000", tagtype="ner")
-
     entity_data = {}
-    
-    
-    
-    
+
+    print("System started\n")
+    print("Completion:")
+    print(0.0, "%")
+    files_done = 0
+
     for folder in dir_names:
-    # ^ replace with 'for folder in dir_names:' for all files ^ ["d0021"]
+    # ^ replace '["d0021"]' with 'for folder in dir_names:' for all files (et vice versa) ^ 
 
-        new_file_cont = []
-
-        #print()
-        #print(folder)
-        #print("_______________________________________________")
-        #print()
-        
+        entity_cont = []
         sentence = []
         sentence_wdata = []
+        complete_cont = []
+
         file_cont = content_dict[folder]
         for word_data in file_cont:
             sentence_wdata.append(word_data)
@@ -75,12 +82,9 @@ def main():
                 sentence.append(data_columns[3])
 
         assigned_ent = ner_tagger.tag(sentence)
-        #print("===============================================")
-        #print(len(sentence_wdata))
-
-        empty_items = 0
 
         # checks for items that do not have a word value
+        empty_items = 0
         for item in sentence_wdata:
             if len(item.split()) < 5:
                 empty_items += 1
@@ -90,32 +94,54 @@ def main():
             if assigned_ent[sent_i][1][:3] != "O":
                 data_c.append(assigned_ent[sent_i][1][:3])
             
-            new_file_cont.append(" ".join(data_c))
+            entity_cont.append(" ".join(data_c))
 
+        j = 0
+        while j < len(entity_cont):
+            word_info = entity_cont[j]
+            info_col = word_info.split()
+            if len(info_col) == 6:
+                if word_info != entity_cont[-1]:
+                    search_for = find_search_term(entity_cont, word_info, [])
+                else:
+                    if len(entity_cont[j - 1].split()) == 6:
+                        search_for = find_search_term(entity_cont, entity_cont[j - 1], [])
+                search_results = wikipedia.search(" ".join(search_for))
+                try:
+                    wiki_page = wikipedia.page(search_results[0])
+                    wikip_url = wiki_page.url
+                except:
+                    wikip_url = "ERROR_OCCURED"
+                info_col.append(wikip_url)
+                if word_info != entity_cont[-1]:
+                    for l in range(len(search_for)):
+                        columns = entity_cont[j + l].split()
+                        columns.append(wikip_url)
+                        complete_cont.append(" ".join(columns))
+                j += len(search_for)
+            else:
+                j += 1
+                complete_cont.append(word_info)
+
+        entity_data[folder] = complete_cont
+
+        # for giving a sence how long it has still to load
         
-            #if len(data_columns) == 6:
-            #    #print(wikipedia.search(word_lemma))
-            #    if len(wikipedia.search(word_lemma)) != 0:
-            #        wikipages = wikipedia.search(word_lemma)
-            #        print(word_lemma)
-            #        print(wikipages)
-            #        print()
-            #        #print(wikipedia.page(wikipages[0]).url)
-            #        #print()
-            #        #data_columns.append(wikipedia.page(wikipages[0]).url)
-            
-            #new_file_cont.append(" ".join(data_columns))
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
+        perc_done = files_done / len(dir_names) * 100
+        print(round(perc_done, 1), "%")
+        files_done += 1
 
-        entity_data[folder] = new_file_cont
 
+    # for showing the contents of entity_data better:
     for k, v in entity_data.items():
         print(k)
+        print("________________________________________")
         print()
-        print(v)
+        for item in v:
+            print(item)
         print()
-
-
-
 
 
 if __name__ == "__main__":
